ed66ba5a6106348b06ca39e360852e298dda877c
Michael Nutt <mnutt346@gmail.com>
2018-12-15 12:24:13 -0600

    Remove required config file in github helper.

--- helpers/github.js
+++ helpers/github.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const config = require('../config.js');
+// const config = require('../config.js');
 const db = require('../database/index.js');
 require('dotenv').config()
 
