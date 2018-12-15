cdcc9bb0646d0d41b29e0f28e423a72802edd284
Michael Nutt <mnutt346@gmail.com>
2018-12-14 20:02:00 -0600

    Render updated top 25 repos when username is submitted.

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
-    <h4> Repo List Component </h4>
+    <h4> Top 25 Repos </h4>
     There are {props.repos.length} repos.
+    <ul>
+      {props.repos.map(repo => {
+        return <li key={repo.repoID}><a href={repo.url}>{repo.repo_name} created by {repo.username}</a></li>
+      })}
+    </ul>
+
   </div>
 )
 
